1a294f01ec2848df084c662b2ef8e5c3827840b0
Alex Bell <abell103@ford.com>
2020-02-06 20:27:07 +0000

    added lambdas module

--- Lambdas/src/com/alexanderbelldev/Main.java
+++ Lambdas/src/com/alexanderbelldev/Main.java
@@ -2,6 +2,6 @@ package com.alexanderbelldev;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("tes");
+        System.out.println("test123");
     }
 }
